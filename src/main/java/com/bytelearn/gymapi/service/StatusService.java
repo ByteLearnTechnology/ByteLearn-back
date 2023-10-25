50d7fe5342b5d514bbc8b3679439a0ee4dd9f360
Alessandro B Lemos Jr <ablemosjunior@gmail.com>
2023-10-25 14:14:37 -0300

    feat: abstração de services

--- src/main/java/com/bytelearn/gymapi/service/StatusService.java
+++ src/main/java/com/bytelearn/gymapi/service/StatusService.java
@@ -1,15 +1,8 @@
 package com.bytelearn.gymapi.service;
 
-import java.util.List;
-
 import com.bytelearn.gymapi.controller.dtos.StatusDTO;
 import com.bytelearn.gymapi.domain.model.Status;
 
-public interface StatusService {
-  Status create(StatusDTO statusDTO);
-  StatusDTO findById(Long id);
-  void delete(Long id);
-  void update(Long id, StatusDTO statusDTO);
-  List<StatusDTO> findAll();
+public interface StatusService extends CrudService<Status, StatusDTO, Long>{
   
 }
